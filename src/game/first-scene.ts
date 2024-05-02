423cf2b704f15fd877672cb2b4c683754ae9f5ee
Matt Newbigging <mrnewbigging@gmail.com>
2024-05-02 14:19:43 +0100

    using new gun class

--- src/game/first-scene.ts
+++ src/game/first-scene.ts
@@ -5,19 +5,14 @@ import { GameLoader } from "../loaders/game-loader";
 import { TextureLoader } from "../loaders/texture-loader";
 import { MouseListener } from "../listeners/mouse-listener";
 import { addGui } from "../utils/utils";
+import { Gun } from "./gun";
 
 export class FirstScene {
   private scene = new THREE.Scene();
   private camera = new THREE.PerspectiveCamera();
-  private raycaster = new THREE.Raycaster();
-
   private controls: PointerLockControls;
 
-  private gun: THREE.Object3D;
-  private canFire = true;
-  private bulletHoleMat: THREE.MeshBasicMaterial;
-  private decalHelper = new THREE.Object3D();
-  private decalSize = new THREE.Vector3(0.1, 0.1, 0.1);
+  private gun: Gun;
 
   constructor(
     private renderer: THREE.WebGLRenderer,
@@ -33,13 +28,8 @@ export class FirstScene {
     this.setupLights();
     this.setupObjects();
     this.gun = this.setupGun();
-    this.bulletHoleMat = this.setupBulletDecal();
 
     this.scene.background = new THREE.Color("#1680AF");
-
-    // I could also just use the 'click' event instead of these two?
-    document.addEventListener("mousedown", this.onMouseDown);
-    document.addEventListener("mouseup", this.onMouseUp);
   }
 
   getCamera() {
@@ -48,7 +38,7 @@ export class FirstScene {
 
   update(dt: number, elapsed: number) {
     // Update
-    this.gunIdle(elapsed);
+    this.gun.update(dt, elapsed);
 
     // Draw
     this.renderer.render(this.scene, this.camera);
@@ -80,24 +70,17 @@ export class FirstScene {
   }
 
   private setupGun() {
-    const { modelLoader, textureLoader } = this.gameLoader;
-
-    const pistol = modelLoader.get("pistol");
-    const pistolTex = textureLoader.get("weapon-26");
-    if (pistolTex) {
-      TextureLoader.applyModelTexture(pistol, pistolTex);
-    }
-
-    // Turn it around since we're looking into scene along negative z
-    pistol.rotateY(Math.PI);
-
-    // Offset relative to camera
-    pistol.position.set(0.45, -0.2, -0.5);
-
-    this.camera.add(pistol);
-    this.scene.add(this.camera);
-
-    return pistol;
+    return new Gun(
+      this.gameLoader,
+      this.mouseListener,
+      this.scene,
+      this.camera,
+      {
+        name: "pistol",
+        firingMode: "semi-auto",
+        rpm: 60,
+      }
+    );
   }
 
   private setupBulletDecal() {
@@ -113,63 +96,5 @@ export class FirstScene {
     });
 
     return material;
-  }
-
-  private gunIdle(elapsedTime: number) {
-    // Bob gun up and down
-    this.gun.position.y += Math.sin(elapsedTime * 2) * 0.00005;
-  }
-
-  private onMouseDown = (e: MouseEvent) => {
-    if (this.canFire) {
-      // Fire
-      this.fireGun();
-      this.canFire = false;
-    }
-  };
-
-  private onMouseUp = (e: MouseEvent) => {
-    // Assumes you can fire as fast as you can click
-    this.canFire = true;
-  };
-
-  private fireGun() {
-    console.log("pew");
-
-    // Animate trigger pull
-    // Spawn bullet
-    // Animate recoil
-
-    // Raycast into scene
-    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
-    const intersections = this.raycaster.intersectObjects(this.scene.children);
-    if (!intersections.length) {
-      return;
-    }
-
-    const hit = intersections[0];
-    if (!hit.face) {
-      return;
-    }
-
-    const mesh = hit.object as THREE.Mesh;
-
-    const normal = hit.face.normal.clone();
-    normal.transformDirection(mesh.matrixWorld);
-    normal.add(hit.point);
-
-    this.decalHelper.position.copy(hit.point);
-    this.decalHelper.lookAt(normal);
-
-    const position = hit.point;
-
-    const decalGeom = new DecalGeometry(
-      mesh,
-      position,
-      this.decalHelper.rotation,
-      this.decalSize
-    );
-    const decal = new THREE.Mesh(decalGeom, this.bulletHoleMat);
-    this.scene.add(decal);
   }
 }
--- src/game/gun.ts
+++ src/game/gun.ts
@@ -1,8 +1,11 @@
 import * as THREE from "three";
+import { DecalGeometry } from "three/examples/jsm/geometries/DecalGeometry";
 import { GameLoader } from "../loaders/game-loader";
 import { MouseListener } from "../listeners/mouse-listener";
+import { TextureLoader } from "../loaders/texture-loader";
 
 export interface GunProps {
+  name: "pistol";
   firingMode: FiringMode;
   rpm: number;
 }
@@ -10,10 +13,14 @@ export interface GunProps {
 export type FiringMode = "semi-auto" | "auto" | "burst";
 
 export class Gun {
-  firingMode: FiringMode;
-  rpm: number;
+  private raycaster = new THREE.Raycaster();
+  private object: THREE.Object3D;
 
-  private canFire = true;
+  private firingMode: FiringMode;
+  private rpm: number;
+  private timeBetweenShots: number;
+  private shotTimer = 0;
+
   private bulletDecalMaterial: THREE.MeshBasicMaterial;
   private decalHelper = new THREE.Object3D();
   private decalSize = new THREE.Vector3(0.1, 0.1, 0.1);
@@ -21,15 +28,53 @@ export class Gun {
   constructor(
     private readonly gameLoader: GameLoader,
     private readonly mouseListener: MouseListener,
+    private readonly scene: THREE.Scene,
+    private readonly camera: THREE.PerspectiveCamera,
     props: GunProps
   ) {
     this.firingMode = props.firingMode;
     this.rpm = props.rpm;
 
+    // RPM determines how many seconds between shots
+    this.timeBetweenShots = 1 / (this.rpm / 60);
+
+    this.object = this.setupGunModel(props.name);
     this.bulletDecalMaterial = this.setupBulletDecalMaterial();
   }
 
-  update() {}
+  update(dt: number, elapsed: number) {
+    // Tick down the shot timer towards 0
+    this.shotTimer -= dt;
+
+    // If holding fire button and shotTimer is finished, we can shoot
+    if (this.mouseListener.lmb && this.shotTimer <= 0) {
+      this.fire();
+
+      // Cannot fire again until timeBetweenShots has passed
+      this.shotTimer = this.timeBetweenShots;
+    } else {
+      this.idle(elapsed);
+    }
+  }
+
+  private setupGunModel(name: string) {
+    const mesh = this.gameLoader.modelLoader.get(name);
+    const texture = this.gameLoader.textureLoader.get("weapon-26");
+    if (texture) {
+      TextureLoader.applyModelTexture(mesh, texture);
+    }
+
+    // Turn it around since we're looking into scene along negative z
+    mesh.rotateY(Math.PI);
+
+    // Offset relative to camera
+    mesh.position.set(0.45, -0.2, -0.5);
+
+    this.camera.add(mesh);
+    this.scene.add(this.camera);
+
+    return mesh;
+  }
 
   private setupBulletDecalMaterial() {
     const decal = this.gameLoader.textureLoader.get("bullet-hole");
@@ -44,5 +89,58 @@ export class Gun {
     });
 
     return material;
+  }
+
+  private idle(elapsed: number) {
+    // Bob up and down slightly
+    this.object.position.y += Math.sin(elapsed * 2) * 0.00005;
+  }
+
+  private fire() {
+    console.log("pew");
+
+    // Was something hit?
+    const hit = this.getIntersection();
+
+    if (hit) {
+      // Draw a decal where we hit
+      this.placeHitDecal(hit);
+    }
+  }
+
+  private getIntersection(): THREE.Intersection | undefined {
+    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
+    const intersections = this.raycaster.intersectObjects(this.scene.children);
+    if (!intersections.length) {
+      return undefined;
+    }
+
+    return intersections[0];
+  }
+
+  private placeHitDecal(hit: THREE.Intersection) {
+    if (!hit.face) {
+      return;
+    }
+
+    const mesh = hit.object as THREE.Mesh;
+
+    const normal = hit.face.normal.clone();
+    normal.transformDirection(mesh.matrixWorld);
+    normal.add(hit.point);
+
+    this.decalHelper.position.copy(hit.point);
+    this.decalHelper.lookAt(normal);
+
+    const position = hit.point;
+
+    const decalGeom = new DecalGeometry(
+      mesh,
+      position,
+      this.decalHelper.rotation,
+      this.decalSize
+    );
+    const decal = new THREE.Mesh(decalGeom, this.bulletDecalMaterial);
+    this.scene.add(decal);
   }
 }

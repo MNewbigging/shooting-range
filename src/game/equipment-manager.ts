6b927d7d285fd1946160cc213cc5f3a1d9a7b4b9
Matt Newbigging <mrnewbigging@gmail.com>
2024-05-09 14:12:12 +0100

    equipment mgr

---
+++ src/game/equipment-manager.ts
@@ -0,0 +1,307 @@
+import * as THREE from "three";
+import * as TWEEN from "@tweenjs/tween.js";
+import { Gun } from "./guns/gun";
+import { TweenFactory, tilAnimEnd } from "./tween-factory";
+import { KeyboardListener } from "../listeners/keyboard-listener";
+import { MouseListener } from "../listeners/mouse-listener";
+import { GameLoader } from "../loaders/game-loader";
+import { TextureLoader } from "../loaders/texture-loader";
+import { GameFactory } from "./game-factory";
+import { EventListener } from "../listeners/event-listener";
+
+export class EquipmentManager {
+  private raycaster = new THREE.Raycaster();
+  private bulletDecalMaterial: THREE.MeshPhongMaterial;
+
+  private tableGuns: Gun[] = []; // in the world
+  private heldGuns: Gun[] = []; // on the player
+  private equippedGun?: Gun; // in player's hands
+  private equipping = false;
+  private lowerAnim?: TWEEN.Tween<any>;
+  private raiseAnim?: TWEEN.Tween<any>;
+
+  constructor(
+    private scene: THREE.Scene,
+    private camera: THREE.PerspectiveCamera,
+    private gameLoader: GameLoader,
+    private keyboardListener: KeyboardListener,
+    private mouseListener: MouseListener,
+    private events: EventListener
+  ) {
+    this.bulletDecalMaterial = GameFactory.getBulletDecalMaterial(
+      this.gameLoader.textureLoader
+    );
+
+    this.mouseListener.addListener("mousedown", this.onMouseDown);
+    this.mouseListener.addListener("wheel", this.onMouseWheel);
+  }
+
+  setup() {
+    this.setupPistol();
+    this.setupRifle();
+  }
+
+  getLookedAtTableGun(): Gun | undefined {
+    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
+
+    for (const gun of this.tableGuns) {
+      const intersections = this.raycaster.intersectObject(gun.object, true);
+      if (intersections.length) {
+        return gun;
+      }
+    }
+
+    return undefined;
+  }
+
+  update(dt: number) {
+    this.equippedGun?.update(dt);
+  }
+
+  lowerEquippedItem() {
+    // Cannot lower what is not equipped
+    if (!this.equippedGun) {
+      return;
+    }
+
+    // Currently lowering
+    if (this.lowerAnim) {
+      return;
+    }
+
+    // If raising, stop
+    this.raiseAnim?.stop();
+    this.raiseAnim = undefined;
+
+    // Cannot use equipped gun while lowered
+    this.equippedGun.disable();
+
+    // Lower the equipped gun
+    this.lowerAnim = TweenFactory.lowerGun(this.equippedGun);
+    this.lowerAnim.start();
+  }
+
+  raiseEquippedItem() {
+    // Cannot raise what isn't equipped
+    if (!this.equippedGun) {
+      return;
+    }
+
+    // Cannot raise if we never lowered
+    if (!this.lowerAnim) {
+      return;
+    }
+
+    // Already raising
+    if (this.raiseAnim) {
+      return;
+    }
+
+    // Stop lowering
+    this.lowerAnim?.stop();
+    this.lowerAnim = undefined;
+
+    // Raise the equipped gun
+    this.raiseAnim = TweenFactory.raiseGun(this.equippedGun);
+    this.raiseAnim.onComplete(() => {
+      this.raiseAnim = undefined;
+      this.equippedGun?.enable();
+    });
+    this.raiseAnim.start();
+  }
+
+  async pickupGun(gun: Gun) {
+    // Begin the pickup animation - move to just below camera
+    const targetPos = this.camera.position.clone();
+    await tilAnimEnd(TweenFactory.pickupGun(gun, targetPos));
+
+    // Remove from table guns
+    this.tableGuns = this.tableGuns.filter((g) => g.object !== gun.object);
+
+    // Add to held guns
+    this.heldGuns.push(gun);
+
+    // Assign hotkey
+    const index = this.heldGuns.length - 1;
+    const key = `${this.heldGuns.length}`;
+    this.keyboardListener.on(key, () => this.onGunHotkey(index));
+
+    // Reset any rotation so it faces the right way
+    gun.object.rotation.set(0, Math.PI, 0);
+
+    // Hide it until ready to show
+    gun.object.visible = false;
+
+    // Equip straight away
+    this.equipGun(gun);
+  }
+
+  private async equipGun(gun: Gun) {
+    if (this.equipping) {
+      return;
+    }
+
+    this.equipping = true;
+
+    // Unequip then hide the current gun
+    if (this.equippedGun) {
+      await this.unequipGun(this.equippedGun);
+    }
+
+    // Add new gun to the camera straight away
+    this.camera.add(gun.object);
+
+    // Position it ready for the show animation
+    gun.object.position.set(gun.holdPosition.x, -1, gun.holdPosition.z);
+    gun.object.rotation.x = -Math.PI;
+
+    // Ensure the gun is visible
+    gun.object.visible = true;
+
+    // Start the show animation
+    await tilAnimEnd(TweenFactory.showGun(gun));
+
+    // This gun is now equipped
+    gun.enable();
+    this.equippedGun = gun;
+    this.equipping = false;
+
+    // In case we're now looking at an interactive item and camera stops...
+    // const lookingAt = this.getLookedAtTableGun();
+    // if (lookingAt) {
+    //   this.lowerEquippedGun();
+    // }
+  }
+
+  private async unequipGun(gun: Gun) {
+    gun.disable();
+    await tilAnimEnd(TweenFactory.hideGun(gun));
+    this.camera.remove(gun.object);
+  }
+
+  private onGunHotkey(index: number) {
+    // Is there a gun for that index?
+    if (this.heldGuns.length <= index) {
+      console.log("not holding gun there");
+      return;
+    }
+
+    const gun = this.heldGuns[index];
+
+    // Are we already holding it?
+    if (this.equippedGun?.id === gun.id) {
+      console.log("already holding it");
+      return;
+    }
+
+    // Swap to that gun so long as we're not currently swapping...
+    this.equipGun(gun);
+  }
+
+  private onMouseWheel = (deltaY: number) => {
+    // Up is negative, down is positive
+
+    // If there are no held guns to swap to, can stop
+    if (this.heldGuns.length < 2) {
+      return;
+    }
+
+    // todo - if nothing is equipped, equip first gun
+
+    const equippedIndex = this.heldGuns.findIndex(
+      (gun) => gun.id === this.equippedGun?.id
+    );
+    let nextIndex = equippedIndex;
+
+    // Up
+    if (deltaY < 0) {
+      // Move to next item in array, wrap around
+      nextIndex =
+        equippedIndex - 1 < 0 ? this.heldGuns.length - 1 : equippedIndex - 1;
+    } else {
+      // Down
+      nextIndex =
+        equippedIndex + 1 === this.heldGuns.length ? 0 : equippedIndex + 1;
+    }
+
+    this.equipGun(this.heldGuns[nextIndex]);
+  };
+
+  private onMouseDown = () => {
+    // Check for left click
+    if (!this.mouseListener.lmb) {
+      return;
+    }
+
+    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
+
+    // Check for clicks on table guns
+    const gun = this.getLookedAtTableGun();
+    if (gun) {
+      this.pickupGun(gun);
+    }
+  };
+
+  private setupPistol() {
+    const pistol = this.gameLoader.modelLoader.pistol;
+
+    // Apply the basic weapon skin to the pistol
+    const texture = this.gameLoader.textureLoader.get("weapon-26");
+    if (texture) {
+      TextureLoader.applyModelTexture(pistol, texture);
+    }
+
+    // Place the pistol object on the table
+    pistol.position.set(0.8, 1.05, 0.5);
+    pistol.rotateY(Math.PI + 0.5);
+    pistol.rotateZ(Math.PI / 2);
+    this.scene.add(pistol);
+
+    // Create the gun class for the pistol
+    const pistolProps = GameFactory.getPistolProps(
+      this.gameLoader,
+      this.bulletDecalMaterial
+    );
+    const pistolGun = new Gun(
+      pistolProps,
+      this.mouseListener,
+      this.keyboardListener,
+      this.events,
+      this.scene,
+      this.camera
+    );
+
+    // Add it to the table guns
+    this.tableGuns.push(pistolGun);
+  }
+
+  private setupRifle() {
+    const rifle = this.gameLoader.modelLoader.rifle;
+
+    const texture = this.gameLoader.textureLoader.get("weapon-26");
+    if (texture) {
+      TextureLoader.applyModelTexture(rifle, texture);
+    }
+
+    rifle.position.set(0.8, 1.05, 1.2);
+    rifle.rotateY(Math.PI - 0.2);
+    rifle.rotateZ(Math.PI / 2);
+    this.scene.add(rifle);
+
+    const rifleProps = GameFactory.getRifleProps(
+      this.gameLoader,
+      this.bulletDecalMaterial
+    );
+
+    const rifleGun = new Gun(
+      rifleProps,
+      this.mouseListener,
+      this.keyboardListener,
+      this.events,
+      this.scene,
+      this.camera
+    );
+
+    this.tableGuns.push(rifleGun);
+  }
+}
--- src/game/game-state.ts
+++ src/game/game-state.ts
@@ -7,11 +7,8 @@ import { MouseListener } from "../listeners/mouse-listener";
 import { KeyboardListener } from "../listeners/keyboard-listener";
 import { EventListener } from "../listeners/event-listener";
 import { RenderPipeline } from "./render-pipeline";
-import { TextureLoader } from "../loaders/texture-loader";
 import { TargetManager } from "./target-manager";
-import { Gun } from "./guns/gun";
-import { TweenFactory, tilAnimEnd } from "./tween-factory";
-import { GameFactory } from "./game-factory";
+import { EquipmentManager } from "./equipment-manager";
 
 export class GameState {
   @observable paused = false;
@@ -22,24 +19,15 @@ export class GameState {
   private renderPipeline: RenderPipeline;
   private clock = new THREE.Clock();
   private controls: PointerLockControls;
-  private raycaster = new THREE.Raycaster();
 
   // Listeners
   private mouseListener: MouseListener;
   private keyboardListener: KeyboardListener;
   private events: EventListener;
 
-  // Constants
-  private bulletDecalMaterial: THREE.MeshPhongMaterial;
-
   // Game
   private targetManager: TargetManager;
-  private tableGuns: Gun[] = []; // in the world
-  private heldGuns: Gun[] = []; // on the player
-  private equippedGun?: Gun; // in player's hands
-  private equipping = false;
-  private lowerAnim?: TWEEN.Tween<any>;
-  private raiseAnim?: TWEEN.Tween<any>;
+  private equipmentManager: EquipmentManager;
 
   constructor(private gameLoader: GameLoader) {
     makeAutoObservable(this);
@@ -54,27 +42,27 @@ export class GameState {
     this.keyboardListener = new KeyboardListener();
     this.events = new EventListener();
 
-    this.mouseListener.addListener("mousedown", this.onMouseDown);
-    this.mouseListener.addListener("wheel", this.onMouseWheel);
-
     // Handle pointer lock events
     document.addEventListener("pointerlockchange", this.onPointerLockChange);
     document.addEventListener("pointerlockerror", this.onPointerLockError);
 
-    // Constants
-    this.bulletDecalMaterial = GameFactory.getBulletDecalMaterial(
-      this.gameLoader.textureLoader
-    );
-
     // Setup game scene
     this.scene.background = new THREE.Color("#1680AF");
     this.setupLights();
-    this.setupPistol();
-    this.setupRifle();
 
     const range = this.gameLoader.modelLoader.shootingRange;
     this.scene.add(range);
     this.targetManager = new TargetManager(range, this.events);
+
+    this.equipmentManager = new EquipmentManager(
+      this.scene,
+      this.camera,
+      this.gameLoader,
+      this.keyboardListener,
+      this.mouseListener,
+      this.events
+    );
+    this.equipmentManager.setup();
 
     this.controls = new PointerLockControls(
       this.camera,
@@ -127,69 +115,6 @@ export class GameState {
     this.scene.add(directLight);
   }
 
-  private setupPistol() {
-    const pistol = this.gameLoader.modelLoader.pistol;
-
-    // Apply the basic weapon skin to the pistol
-    const texture = this.gameLoader.textureLoader.get("weapon-26");
-    if (texture) {
-      TextureLoader.applyModelTexture(pistol, texture);
-    }
-
-    // Place the pistol object on the table
-    pistol.position.set(0.8, 1.05, 0.5);
-    pistol.rotateY(Math.PI + 0.5);
-    pistol.rotateZ(Math.PI / 2);
-    this.scene.add(pistol);
-
-    // Create the gun class for the pistol
-    const pistolProps = GameFactory.getPistolProps(
-      this.gameLoader,
-      this.bulletDecalMaterial
-    );
-    const pistolGun = new Gun(
-      pistolProps,
-      this.mouseListener,
-      this.keyboardListener,
-      this.events,
-      this.scene,
-      this.camera
-    );
-
-    // Add it to the table guns
-    this.tableGuns.push(pistolGun);
-  }
-
-  private setupRifle() {
-    const rifle = this.gameLoader.modelLoader.rifle;
-
-    const texture = this.gameLoader.textureLoader.get("weapon-26");
-    if (texture) {
-      TextureLoader.applyModelTexture(rifle, texture);
-    }
-
-    rifle.position.set(0.8, 1.05, 1.2);
-    rifle.rotateY(Math.PI - 0.2);
-    rifle.rotateZ(Math.PI / 2);
-    this.scene.add(rifle);
-
-    const rifleProps = GameFactory.getRifleProps(
-      this.gameLoader,
-      this.bulletDecalMaterial
-    );
-
-    const rifleGun = new Gun(
-      rifleProps,
-      this.mouseListener,
-      this.keyboardListener,
-      this.events,
-      this.scene,
-      this.camera
-    );
-
-    this.tableGuns.push(rifleGun);
-  }
-
   private update = () => {
     requestAnimationFrame(this.update);
 
@@ -200,8 +125,8 @@ export class GameState {
       // Targets
       this.targetManager.update(dt);
 
-      // Active item
-      this.equippedGun?.update(dt);
+      // Equipment
+      this.equipmentManager.update(dt);
 
       // Animations
       TWEEN.update();
@@ -212,217 +137,28 @@ export class GameState {
   };
 
   private onCameraMove = () => {
-    // First clear any outlines
+    // First clear any outlines for this frame
     this.renderPipeline.clearOutlines();
 
     // Are we looking at a table gun?
-    const gun = this.getLookedAtTableGun();
+    const gun = this.equipmentManager.getLookedAtTableGun();
     if (gun) {
       this.renderPipeline.outlineObject(gun.object);
-      this.lowerEquippedGun();
+      this.equipmentManager.lowerEquippedItem();
     } else {
-      this.stopLoweringEquippedGun();
-    }
-  };
-
-  private getLookedAtTableGun(): Gun | undefined {
-    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
-
-    for (const gun of this.tableGuns) {
-      const intersections = this.raycaster.intersectObject(gun.object, true);
-      if (intersections.length) {
-        return gun;
-      }
+      this.equipmentManager.raiseEquippedItem();
     }
 
-    return undefined;
-  }
-
-  private lowerEquippedGun() {
-    // Cannot lower what is not equipped
-    if (!this.equippedGun) {
-      return;
-    }
-
-    // Currently lowering
-    if (this.lowerAnim) {
-      return;
-    }
-
-    // If raising, stop
-    this.raiseAnim?.stop();
-    this.raiseAnim = undefined;
-
-    // Cannot use equipped gun while lowered
-    this.equippedGun.disable();
-
-    // Lower the equipped gun
-    this.lowerAnim = TweenFactory.lowerGun(this.equippedGun);
-    this.lowerAnim.start();
-  }
-
-  private stopLoweringEquippedGun() {
-    // Cannot raise what isn't equipped
-    if (!this.equippedGun) {
-      return;
-    }
-
-    // Cannot raise if we never lowered
-    if (!this.lowerAnim) {
-      return;
-    }
-
-    // Already raising
-    if (this.raiseAnim) {
-      return;
-    }
-
-    // Stop lowering
-    this.lowerAnim?.stop();
-    this.lowerAnim = undefined;
-
-    // Raise the equipped gun
-    this.raiseAnim = TweenFactory.raiseGun(this.equippedGun);
-    this.raiseAnim.onComplete(() => {
-      this.raiseAnim = undefined;
-      this.equippedGun?.enable();
-    });
-    this.raiseAnim.start();
-  }
-
-  private onMouseDown = () => {
-    // Check for left click
-    if (!this.mouseListener.lmb) {
-      return;
-    }
-
-    this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera);
-
-    // Check for clicks on table guns
-    for (const gun of this.tableGuns) {
-      const intersections = this.raycaster.intersectObject(gun.object);
-      if (intersections.length) {
-        // Pick it up
-        this.pickupGun(gun);
-        return;
-      }
-    }
-  };
-
-  private async pickupGun(gun: Gun) {
-    // Begin the pickup animation - move to just below camera
-    const targetPos = this.camera.position.clone();
-    await tilAnimEnd(TweenFactory.pickupGun(gun, targetPos));
-
-    // Remove from table guns
-    this.tableGuns = this.tableGuns.filter((g) => g.object !== gun.object);
-
-    // Add to held guns
-    this.heldGuns.push(gun);
-
-    // Assign hotkey
-    const index = this.heldGuns.length - 1;
-    const key = `${this.heldGuns.length}`;
-    this.keyboardListener.on(key, () => this.onGunHotkey(index));
-
-    // Reset any rotation so it faces the right way
-    gun.object.rotation.set(0, Math.PI, 0);
-
-    // Hide it until ready to show
-    gun.object.visible = false;
-
-    // Equip straight away
-    this.equipGun(gun);
-  }
-
-  private async equipGun(gun: Gun) {
-    if (this.equipping) {
-      return;
-    }
-
-    this.equipping = true;
-
-    // Unequip then hide the current gun
-    if (this.equippedGun) {
-      await this.unequipGun(this.equippedGun);
-    }
-
-    // Add new gun to the camera straight away
-    this.camera.add(gun.object);
-
-    // Position it ready for the show animation
-    gun.object.position.set(gun.holdPosition.x, -1, gun.holdPosition.z);
-    gun.object.rotation.x = -Math.PI;
-
-    // Ensure the gun is visible
-    gun.object.visible = true;
-
-    // Start the show animation
-    await tilAnimEnd(TweenFactory.showGun(gun));
-
-    // This gun is now equipped
-    gun.enable();
-    this.equippedGun = gun;
-    this.equipping = false;
-
-    // In case we're now looking at an interactive item and camera stops...
-    const lookingAt = this.getLookedAtTableGun();
-    if (lookingAt) {
-      this.lowerEquippedGun();
-    }
-  }
-
-  private async unequipGun(gun: Gun) {
-    gun.disable();
-    await tilAnimEnd(TweenFactory.hideGun(gun));
-    this.camera.remove(gun.object);
-  }
-
-  private onGunHotkey(index: number) {
-    // Is there a gun for that index?
-    if (this.heldGuns.length <= index) {
-      console.log("not holding gun there");
-      return;
-    }
-
-    const gun = this.heldGuns[index];
-
-    // Are we already holding it?
-    if (this.equippedGun?.id === gun.id) {
-      console.log("already holding it");
-      return;
-    }
-
-    // Swap to that gun so long as we're not currently swapping...
-    this.equipGun(gun);
-  }
-
-  private onMouseWheel = (deltaY: number) => {
-    // Up is negative, down is positive
-
-    // If there are no held guns to swap to, can stop
-    if (this.heldGuns.length < 2) {
-      return;
-    }
-
-    // todo - if nothing is equipped, equip first gun
-
-    const equippedIndex = this.heldGuns.findIndex(
-      (gun) => gun.id === this.equippedGun?.id
-    );
-    let nextIndex = equippedIndex;
-
-    // Up
-    if (deltaY < 0) {
-      // Move to next item in array, wrap around
-      nextIndex =
-        equippedIndex - 1 < 0 ? this.heldGuns.length - 1 : equippedIndex - 1;
-    } else {
-      // Down
-      nextIndex =
-        equippedIndex + 1 === this.heldGuns.length ? 0 : equippedIndex + 1;
-    }
-
-    this.equipGun(this.heldGuns[nextIndex]);
+    /**
+     * Slightly odd control above - the mgr has the lookedAtGun, but we need it here
+     * and we're telling mgr how to react to its own knowledge...
+     *
+     * This class needs to know whether to outline something
+     * The mgr needs to know whether to raise or lower an item
+     *
+     * Listening to camera move is better places at this high level, for control
+     *
+     * I could have the mgr provide an event for 'looking at table gun' but it's a bit specific...
+     */
   };
 }
